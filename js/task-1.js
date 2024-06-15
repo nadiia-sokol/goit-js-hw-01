c101ee8ef8fa3a0732fbc8762816b91ba4b798da
Nadiia Sokol <dianadin@ukr.net>
2024-06-15 19:20:40 +0200

    add2

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,8 @@
 'use strict';
 function makeTransaction (quantity, pricePerDroid) {
     const totalPrice = quantity * pricePerDroid;  
-    console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!`);
-    return totalPrice;
+    const message = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+    return message;
 }
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
